fb5d2d3801a13792b767e1e05d096e6fdb3b8bee
pavanboddu7052 <68012336+pavanboddu7052@users.noreply.github.com>
2026-08-07 16:43:10 +0530

    Add files via upload

---
+++ photos.js
@@ -0,0 +1,14 @@
+window.WEDDING_CONFIG = {
+  photos: [
+    "KCP01263 copy.jpg",
+    "KCP01268 copy.jpg",
+    "KCP01280 copy.jpg",
+    "KCP01359 copy.jpg",
+    "KCP02284 copy.jpg",
+    "KCP01264 copy.jpg",
+    "KCP02291 copy.jpg",
+    "KCP02311 copy.jpg"
+  ],
+  // Set to your mp3 filename once added (e.g. "song.mp3")
+  musicFile: "Wedding.mp3"
+};
