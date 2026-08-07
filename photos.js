82d5e52f4e7f2f203f05b09d7fb493f98fba48ea
pavanboddu7052 <68012336+pavanboddu7052@users.noreply.github.com>
2026-08-07 19:25:11 +0530

    Update wedding photos in WEDDING_CONFIG
    Removed old photo filenames and added new RAW images.

--- photos.js
+++ photos.js
@@ -1,13 +1,7 @@
 window.WEDDING_CONFIG = {
   photos: [
-    "KCP01263 copy.jpg",
-    "KCP01268 copy.jpg",
-    "KCP01280 copy.jpg",
-    "KCP01359 copy.jpg",
-    "KCP02284 copy.jpg",
-    "KCP01264 copy.jpg",
-    "KCP02291 copy.jpg",
-    "KCP02311 copy.jpg"
+    "RAW1.jpeg",
+    "RAW2.jpeg"
   ],
   // Set to your mp3 filename once added (e.g. "song.mp3")
   musicFile: "Wedding.mp3"
