959719c70d3f060a0120591c9898a8b48ac062d6
pavanboddu7052 <68012336+pavanboddu7052@users.noreply.github.com>
2026-08-18 01:16:23 +0530

    Add more photos to WEDDING_CONFIG
    Added additional photo filenames to the wedding configuration.

--- Couplepics/photos.js
+++ Couplepics/photos.js
@@ -1,7 +1,16 @@
 window.WEDDING_CONFIG = {
   photos: [
     "RAW1.jpeg",
-    "RAW2.jpeg"
+    "RAW2.jpeg",
+    "RAW3.jpeg",
+    "RAW4.jpeg",
+    "RAW5.jpeg",
+    "RAW6.jpeg",
+    "RAW7.jpeg",
+    "RAW8.jpeg",
+    "RAW9.jpeg",
+    "RAW10.jpeg",
+    "RAW11.jpeg"
   ],
   // Set to your mp3 filename once added (e.g. "song.mp3")
   musicFile: "Wedding.mp3"
